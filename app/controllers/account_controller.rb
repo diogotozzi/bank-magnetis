89d428f533cd9a1b4ba278641df88d899839ceeb
Diogo Tozzi <tozzi.diogo@gmail.com>
2019-07-20 20:54:20 -0300

    Calculating the fund more precisely

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,11 +1,16 @@
 class AccountController < ActionController::API
     def show
       account = Account.find(params[:id])
-      transfers = Transfer.where(to_account_id: params[:id])
+      transfers_to = Transfer.where(to_account_id: params[:id])
+      transfers_from = Transfer.where(from_account_id: params[:id]).where.not(to_account_id: params[:id])
 
       sum = 0
-      transfers.each do |fund|
+      transfers_to.each do |fund|
         sum += fund.qty
+      end
+
+      transfers_from.each do |fund|
+        sum -= fund.qty
       end
 
       render :json => {:funds => sum}, status: :ok
